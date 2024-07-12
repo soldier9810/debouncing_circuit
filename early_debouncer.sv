0c1890ad9eabe7b92423c5174eacdd587ca8ae00
Aryan Gupta <133555017+soldier9810@users.noreply.github.com>
2024-07-12 10:27:42 +0530

    Add files via upload

--- early_debouncer.sv
+++ early_debouncer.sv
@@ -28,16 +28,20 @@ module early_debouncer(
     logic ms10;
     counter_10ms(.*);
     
-    typedef enum {one, wait1_10, wait1_20, zero} state_type;
+    typedef enum logic [3:0]{one, wait1_10, wait1_20, wait1_30, zero, wait0_10, wait0_20, wait0_30} state_type;
     
     state_type current_state, next_state;
     
     always_comb begin
         case(current_state)
-            zero: next_state = (sw) ? one : zero;
-            one: next_state = (ms10) ? wait1_10 : one;
+            zero: next_state = (sw) ? wait1_10 : zero;
             wait1_10 : next_state = (ms10) ? wait1_20 : wait1_10;
-            wait1_20: next_state = (sw) ? wait1_20 : zero;
+            wait1_20: next_state = (ms10) ? wait1_30 : wait1_20;
+            wait1_30: next_state = (ms10) ? one : wait1_30;
+            one: next_state = (sw) ? one : wait0_10;
+            wait0_10 : next_state = (ms10) ? wait0_20 : wait0_10;
+            wait0_20: next_state = (ms10) ? wait0_30 : wait0_20;
+            wait0_30: next_state = (ms10) ? zero : wait1_30;
             default: next_state = zero;
         endcase
     end
@@ -47,6 +51,6 @@ module early_debouncer(
         else current_state <= next_state;
     end
     
-    assign db = (current_state == zero) ? 1'b0 : 1'b1; 
+    assign db = current_state == one | current_state == wait1_10 | current_state == wait1_20  | current_state == wait1_30;
     
 endmodule
